db770ba322e7e670f765d25f68fce6e78c3c7151
i55 <i@DESKTOP-G7TCLEJ>
2019-05-10 10:23:38 +0800

    文件

---
+++ src/components/ClientWeb/TextTemplate/PlayNewsMsg.vue
@@ -0,0 +1,26 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "PlayNewsMsg",
+        data()
+        {
+            return{
+
+            }
+        },
+        methods:{
+
+        },
+        mounted(){
+            this.ProjectName=this.$route.params.toconfigtable;//接收唯一标识id
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
