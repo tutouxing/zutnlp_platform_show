db1e114a8f7de7e237bdb6d7b1db36d2b323374f
i55 <i@DESKTOP-G7TCLEJ>
2019-05-11 15:41:17 +0800

    二级

--- src/components/ClientWeb/TextTemplate/PlayContentMsg.vue
+++ src/components/ClientWeb/TextTemplate/PlayContentMsg.vue
@@ -1,30 +1,32 @@
 <template>
-    <h3>{{AllMsg.TextMsg.HeadLine}}</h3>
-    <p style="margin-top: 20%">{{AllMsg.TextMsg.TextContent}} </p>
+    <div  style="text-align:center; width: 520px; margin-left: 30%" >
+    <h2>{{AllMsg.TextMsg[0].HeadLine}}</h2>
+    <p>{{AllMsg.TextMsg[0].TextContent}} </p>
+    </div>
 </template>
 
 <script>
     export default {
         name: "PlayContentMsg",
         data(){
-           return {
-               AllMsg:{
-                   TextMsg:[
-                       {
-                           HeadLine:   标题   ,//大字标题
-                           TextContent:   sdsadasdasdasdasda   ,//文本内容
-                           Iconograph:      ,//插图
-                       }
-                   ],
+            return {
+                AllMsg:{
+                    TextMsg:[
+                        {
+                            HeadLine:'标题'    ,//大字标题
+                            TextContent:'sdsadasdasdasdasda',//文本内容
+                            Iconograph:'',//插图
+                        }
+                    ],
 
-               }
-           }
+                }
+            }
         },
         methods:{
 
         },
         mounted(){
-            this.ProjectName=this.$route.params.toconfigtable;//接收唯一标识id
+             this.AllMsg.TextMsg[1]=this.$route.params.items;//接收唯一标识id
         }
     }
 </script>
