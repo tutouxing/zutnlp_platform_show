30eb9c77239454ff14f7ef1afb3f6a365bde8600
i55 <i@DESKTOP-G7TCLEJ>
2019-05-14 18:33:14 +0800

    模板提交2

--- src/components/ClientWeb/TextTemplate/FrameWork.vue
+++ src/components/ClientWeb/TextTemplate/FrameWork.vue
@@ -1,130 +1,4 @@
- <!--
-<template>
-    <el-container>
-        <el-header>
-&lt;!&ndash;
-          <img :src="Allofdata.Pictures.HeaderPictureEarth"  style="width: 100%"/>
-&ndash;&gt;    <div>
 
-            <el-tabs v-model="activeName" @tab-click="handleClick">
-                <el-tab-pane label="体育" name="first">
-                    <div>
-                        <el-container>
-                            <el-aside>
-                                <h3 style="margin-top: 10px;margin-left: 5px" v-for="item in Allofdata.TextMessage">
-                                    {{item.title}}
-                                </h3>
-                            </el-aside>
-                            <el-main>
-                                <div class="block">
-                                    <el-carousel  height="260px" style="width: 525px;">
-                                        <el-carousel-item v-for="item in Allofdata.Pictures" :key="item">
-                                            <h3>{{ item }}</h3>
-                                            <img :src="item" />
-                                        </el-carousel-item>
-                                    </el-carousel>
-                                </div>
-                            </el-main>
-                            <div class="tiyuaside">
-                                <h3 style="margin-top: 10px;margin-left: 5px" v-for="item in Allofdata.TextMessage">
-                                    {{item.title}}
-                                </h3>
-                            </div>
-                        </el-container>
-                        <el-container>
-                            <el-aside>
-                                <h3 style="margin-top: 10px;margin-left: 5px" v-for="item in Allofdata.TextMessage">
-                                    {{item.title}}
-                                </h3>
-                            </el-aside>
-                            <el-main>视频</el-main>
-                            <div class="tiyuaside">
-                                <h3 style="margin-top: 10px;margin-left: 5px" v-for="item in Allofdata.TextMessage">
-                                    {{item.title}}
-                                </h3>
-                            </div>
-                        </el-container>
-                        <el-container>
-                            <el-aside>
-                                <h3 style="margin-top: 10px;margin-left: 5px" v-for="item in Allofdata.TextMessage">
-                                    {{item.title}}
-                                </h3>
-                            </el-aside>
-                            <el-main>
-                                <video width="500" height="255" controls="controls" autoplay="autoplay">
-                                  <source src="/i/movie.ogg" type="video/ogg" />
-                                <source src="/i/movie.mp4" type="video/mp4" />
-                                  <source src="/i/movie.webm" type="video/webm" />
-                                  <object data="/i/movie.mp4" width="500" height="255">
-                                        <embed width="500" height="255" src="/i/movie.swf" />
-                                      </object>
-                                </video>
-                            </el-main>
-                            <div class="tiyuaside">
-                                <h3 style="margin-top: 10px;margin-left: 5px" v-for="item in Allofdata.TextMessage">
-                                    {{item.title}}
-                                </h3>
-                            </div>
-                        </el-container>
-
-                    </div>&lt;!&ndash;体育&ndash;&gt;</el-tab-pane>
-                <el-tab-pane label="科技" name="second">
-                    <div>
-                    <el-carousel :interval="4000" type="card" height="200px">
-                        <el-carousel-item v-for="item in Allofdata.Pictures" :key="item">
-                            <img :src="item" />
-                            <h3>{{ item }}</h3>
-                        </el-carousel-item>
-                    </el-carousel>
-                </div>
-                    <div>
-
-                        <div class="divmain" style="float:left;" v-for="item in Allofdata.TextMessage">
-                            <img style="margin-top: 15px"  class="photo" :src="item.picture" />
-                            <p >{{item.title}}</p>
-                        </div>
-
-                        <div class="divaside" width="500px"  v-for="item in Allofdata.OnlyText">
-                            <p class="div-inline">{{item.introduction}}</p>
-                        </div >
-                    </div>
-                </el-tab-pane>
-                <el-tab-pane label="娱乐" name="third">
-                    <div>
-
-                        <div class="divmain" style="float:left;" v-for="item in Allofdata.TextMessage">
-                            <img style="margin-top: 15px"  class="photo" :src="item.picture" />
-                            <img style="margin-top: 15px"  class="photo" :src="item.picture" />
-                            <img style="margin-top: 15px"  class="photo" :src="item.picture" />
-                            <p >{{item.title}}</p>
-                        </div>
-
-                    </div>
-
-                </el-tab-pane>
-                <el-tab-pane label="民生" name="fourth">民生</el-tab-pane>
-            </el-tabs>
-
-        </div>
-        </el-header>
-
-
-        &lt;!&ndash;   <el-main v-for="item in Allofdata.TextMessage">
-
-                    <div @click="sparamtocontent(item)">
-                        <img class="photo" :src="item.picture" />
-                        <p>{{item.title}}</p>
-                    </div>
-
-                </el-main>
-                <el-aside>Aside</el-aside>&ndash;&gt;
-
-        <el-footer>Footer</el-footer>
-
-
-    </el-container>
-</template>
--->
 <template>
     <el-container style="margin-top: 10px">
     <el-header>
@@ -135,12 +9,12 @@
                              <el-container>
                                  <el-aside>
                                    <h3 style="margin-top: 10px;margin-left: 5px" v-for="item in Allofdata.TiYuMsG.Left" v-on:click="tiyutonext1(item)">
-                                       {{item.Title}}
+                                       {{item.title}}
                                    </h3>
                                  </el-aside>
 
                                  <el-main>
-                                    <div class="block">
+                                    <div>
                                         <!-- <span class="demonstration">默认 Hover 指示器触发</span>     -->
                                         <el-carousel>
                                             <el-carousel-item v-for="item in Allofdata.TiYuMsG.Pictures" :key="item"  v-on:click="tiyutonext0(item)">
@@ -152,7 +26,7 @@
                                  </el-main>
                                  <div class="tiyuaside">
                                    <h3 style="margin-top: 10px;margin-left: 5px" v-for="item in Allofdata.TiYuMsG.Right" v-on:click="tiyutonext1(item)">
-                                       图片侧栏{{item.Title}}
+                                       图片侧栏{{item.title}}
                                    </h3>
                                  </div>
                                   </el-container>
@@ -193,9 +67,9 @@
                                    <el-tab-pane label="科技" name="second">
                             <div>
                                <el-carousel :interval="4000" type="card" height="200px">
-                                       <el-carousel-item v-for="item in Allofdata.KeJiMsg.Pictures.Picture" :key="item">
-                                               <img :src="item"  v-on:click="kejigonext0(item)" />
-                                               <h3>{{ item }}</h3>
+                                       <el-carousel-item v-for="item in Allofdata.KeJiMsg.Pictures" :key="item">
+                                               <img :src="item.Picture"  v-on:click="kejigonext0(item)" />
+                                               <h3>{{ item .Picture}}</h3>
                                            </el-carousel-item>
                                    </el-carousel>
                            </div>
@@ -225,10 +99,10 @@
                                     </div>
                            <div class="yule2">
                                <div class="yule2-1">
-                                   <div class="block">
+                                   <div>
                                        <el-carousel >
                                            <el-carousel-item v-for="item in Allofdata.YuLeMsg.Pictures" :key="item" >
-                                               <img :src="item.Picture"   height="210px" style="width: 500px;" v-on:click="yuletonext2(item)" />
+                                               <img :src="item.Picture"   height="200px" style="width: 500px;" v-on:click="yuletonext2(item)" />
                                                <p>{{item.Introduction}}</p>
                                            </el-carousel-item>
                                        </el-carousel>
@@ -241,7 +115,7 @@
                            <div class="yule3" style="float: bottom;"  v-for="item in Allofdata.YuLeMsg.PicturesAndText" v-on:click="yuletonext0(item)">
 
                                 <div>
-                                    <p>{{item.Text.Title}}</p>
+                                    <p>{{item.Text.title}}</p>
                                     <img style="margin-top: 15px"  class="yulephoto" :src="item.Pictures[0].Picture" />
                                     <img style="margin-top: 15px"  class="yulephoto" :src="item.Pictures[1].Picture" />
                                     <img style="margin-top: 15px"  class="yulephoto" :src="item.Pictures[2].Picture" />
@@ -261,6 +135,7 @@
            </el-container>
 </template>
 <script>
+    import state from '../../../store/state.js'
     export default {
         name: "FrameWork",
 
@@ -281,16 +156,16 @@
                         ],
                         PictureAndText:[
                             {
-                                content:'',//内容
+                                Content:'内容',//内容
                                 picture:'../Picture/earth.png' ,//插图
-                                type:'',//类型
+                                type:'类型',//类型
                                 title:' 科技新闻太阳科技新闻太阳科技新闻太阳 '  ,
                             }
                         ],
                         OnlyText:[
                             {
-                                content:'科技新闻太阳科技新闻太阳科技新闻太阳'      ,//内容
-                                type:''  ,//类型
+                                Content:'科技新闻太阳科技新闻太阳科技新闻太阳'      ,//内容
+                                type:'类型'  ,//类型
                                 title:' 科技新闻太阳科技新闻太阳科技新闻太阳'  ,
                             }
                         ]
@@ -305,10 +180,16 @@
                                 Pictures:[
                                     {
                                         Picture:'../Picture/earth.png '
+                                    },
+                                    {
+                                        Picture:'../Picture/earth.png '
+                                    },
+                                    {
+                                        Picture:'../Picture/earth.png '
                                     }
                                 ],
                                 Text:{
-                                    Title:'地球',
+                                    title:'地球',
                                     Introduction:'这是地球啊',
                                     Content:'地球好',
                                 }
@@ -327,12 +208,12 @@
                         ],
                         Recommend:[
                             {
-                                Title:'科技新闻太阳科技新闻太阳科技新闻太阳',
+                                title:'科技新闻太阳科技新闻太阳科技新闻太阳',
                                 Content:'科技新闻太阳科技新闻太阳科技新闻太阳',
                                 Author:'w',
                             },
                             {
-                                Title:'科技新闻太阳科技新闻太阳科技新闻太阳',
+                                title:'科技新闻太阳科技新闻太阳科技新闻太阳',
                                 Content:'科技新闻太阳科技新闻太阳科技新闻太阳',
                                 Author:'w',
                             }
@@ -343,13 +224,13 @@
 
                         Left:[
                             {
-                                Title:'火箭总冠军',
+                                title:'火箭总冠军',
                                 Content:'哈登mvp'
                             }
                         ],
                         Right:[
                             {
-                                Title:'火箭总冠军',
+                                title:'火箭总冠军',
                                 Content:'哈登mvp'
 
                             }
@@ -381,7 +262,7 @@
 
 
 
-                    TextMessage:[
+                  /*  TextMessage:[
                         {
                             content:''      ,//内容
                             picture:  ' ../Picture/earth.png  ' ,//插图
@@ -403,9 +284,10 @@
                          HeaderPictureEarth:  '../Picture/earth.png '   ,
                          HeaderPictureMoon:  ' ../Picture/moon.png'   ,
                          HeaderPictureSun:  '../Picture/sun.png '   ,
-                     },//图片轮播用
+                     },*///图片轮播用
                 },
                 activeName:  'second' ,
+                gotdata:{},
             }
       },
         methods:{
@@ -426,7 +308,7 @@
                 console.log("打印的科技1");//展示图片带文字
             },
             kejigonext2(item){
-                console.log(item.introduction);
+                console.log(item.title);
                 console.log("打印的科技2");//仅仅展示文字
                 this.$router.push({name:'PlayContentMsg',params:{items:item}});//用路由传递参数唯一标识
                 this.$router.push({path:'/PlayContentMsg'});
@@ -469,7 +351,7 @@
                 this.$router.push({path:'/PlayVideosMsg'});
                 //点击视频跳转
             },
-                sparamtocontent(item){
+              /*  sparamtocontent(item){
                     this.$router.push({name:'PlayContentMsg',params:{id:id}});//用路由传递参数唯一标识
                 },
                 passparamtonewmsg(){
@@ -478,13 +360,17 @@
                 passparamtopicturemsg(){
                     this.$router.push({name:'PlayPictrueMsg',params:{id:id}});//用路由传递参数唯一标识
                 },
-
+*/
                 handleClick(tab, event) {
                     console.log(tab, event);
                 },
 
         },
         mounted(){
+           this.gotdata= state.channel;
+                console.log( this.gotdata);
+        }
+      /*  mounted(){
 
                let that=this;
                let url=  ''    ;
@@ -494,7 +380,7 @@
                      console.log(response.data[j]);
                  }
                  that.data().Allofdatae.TextMessage=response.data;
-              /*  for (let j in response.data) {
+              /!*  for (let j in response.data) {
                    that.alltablename.names.push(
                         {
                             value:response.data[j].tablename,
@@ -502,126 +388,16 @@
                             key: Date.now()
                         }
                     );
-                }*/
+                }*!/
             }).catch(function (error) {
                 console.log(error);
             });
-        }
+        }*/
     }
 </script>
 
-<!--
+
 <style scoped>
-    .tiyuaside{
-        width: 300px;
-        height: 300px;
-        margin-top: 5%;
-        background: blue;
-        margin-right: 5%;
-        cursor:pointer;/*鼠标放上去是一个手*/
-    }
-    .  el-tab-pane{
-        background: aqua;
-    }
-    p:hover{
-         color: aquamarine;
-     }
-    h3:hover{
-        color: aquamarine;
-    }
-    img:hover{
-        transform: scale(1.4);
-        transition: all 0.6s;
-    }
-    .divmain{
-        box-shadow: 0 10px 10px rgba(0, 0, 0, .12), 0 0 60px rgba(0, 0, 0, .04);
-        word-wrap:break-word ;
-        border-radius: 4px;
-        cursor:pointer;/*鼠标放上去是一个手*/
-
-        align:center;
-        margin-left: 16% ;
-        height: 150px;
-        width: 500px;
-        overflow:auto;
-        border-top :1px solid darkgray;
-        border-bottom:1px solid darkgray;
-    }
-    .divaside{
-        box-shadow: 0 10px 10px rgba(0, 0, 0, .12), 0 0 60px rgba(0, 0, 0, .04);
-        word-wrap:break-word ;
-        border-radius: 4px;
-        cursor:pointer;/*鼠标放上去是一个手*/
-
-        height: 150px;
-        width:500px;
-        margin-left: 60%;
-
-        overflow:auto;
-
-        text-align: center;
-        line-height: 200px;
-
-        border-top :1px solid darkgray;
-        border-bottom:1px solid darkgray;
-        /*   display:inline-block;不能加   这个和 overflow:auto;冲突*/
-    }
-    .photo{
-        float:left;
-        width: 200px;
-        height:100px;
-    }
-    boder{
-        border-radius: 4px;
-        border-left:1px solid #000;
-        border-right:1px solid #000;
-        border-top :1px solid #000;
-        border-bottom:1px solid #000;
-    }
-    .el-aside{
-        box-shadow: 0 10px 10px rgba(0, 0, 0, .12), 0 0 60px rgba(0, 0, 0, .04);
-        word-wrap:break-word ;
-        border-radius: 4px;
-        height: 300px;
-        margin-left: 5%;
-        margin-top: 5%;
-        /*  background-color: #D3DCE6;*/
-        /*  background: black;*/
-        color: #333;
-        cursor:pointer;/*鼠标放上去是一个手*/
-    }
-    .el-main{
-        box-shadow: 0 10px 10px rgba(0, 0, 0, .12), 0 0 60px rgba(0, 0, 0, .04);
-        word-wrap:break-word ;
-        border-radius: 4px;
-        background: red;
-        margin-top: 5%;
-        cursor:pointer;/*鼠标放上去是一个手*/
-        /*     display:inline-block;*/
-        height: 300px;
-        width: 300px;
-    }
-    .el-carousel{
-        margin-left: 15%;
-    }
-
-    .el-carousel__item h3 {
-        color: #475669;
-        font-size: 14px;
-        opacity: 0.75;
-        line-height: 200px;
-        margin: 0;
-    }
-
-    .el-carousel__item:nth-child(2n) {
-        background-color: #99a9bf;
-    }
-
-    .el-carousel__item:nth-child(2n    1) {
-        background-color: #d3dce6;
-    }
-</style>-->
-<style>
     h4:hover{
         cursor:pointer;
         text-decoration:underline;color: brown;
@@ -648,7 +424,7 @@
     }
     .yule2{
         width: 500px;
-        height: 300px;
+        height: 301px;
         overflow:auto;
         margin-left: 50%;
         cursor:pointer;
@@ -765,7 +541,7 @@
         width:500px;
         margin-left: 60%;
 
-        overflow:auto;
+       /* overflow:auto;*/
 
         text-align: center;
         line-height: 200px;
@@ -804,7 +580,7 @@
         border-radius: 4px;
         /*  background: red;*/
         margin-top: 5%;
-        /*     display:inline-block;*/
+        /*     display:inline-block;360*/
         height: 350px;
         width: 300px;
         cursor:pointer;/*鼠标放上去是一个手*/
--- src/router/router.js
+++ src/router/router.js
@@ -12,10 +12,14 @@ import createcode from '../components/FormConfig/createcode.vue'
 import modeDir from '../components/modeFirst/modeDir.vue'
 import modeCont from '../components/modeFirst/modeCont.vue'
 
-/*import Framework from '../components/ClientWeb/TextTemplate/FrameWork'
-import PlayContentMsg from '../components/ClientWeb/TextTemplate/PlayContentMsg'
-import PlayNewsMsg from '../components/ClientWeb/TextTemplate/PlayNewsMsg'
-import PlayPictureMsg from '../components/ClientWeb/TextTemplate/PlayPictureMsg'*/
+
+
+import PlayManyPicturesContentMsg from '../components/ClientWeb/TextTemplate/PlayManyPicturesContentMsg.vue'
+import PlayVideosMsg from '../components/ClientWeb/TextTemplate/PlayVideosMsg.vue'
+import FrameWork from '../components/ClientWeb/TextTemplate/FrameWork.vue'
+import PlayContentMsg from '../components/ClientWeb/TextTemplate/PlayContentMsg.vue'
+import PlayNewsMsg from '../components/ClientWeb/TextTemplate/PlayNewsMsg.vue'
+import PlayPictrueMsg from '../components/ClientWeb/TextTemplate/PlayPictrueMsg'
 /*
 *
 *
@@ -157,11 +161,50 @@ export default[
             component:createcode,
 
         },
+
+
+
+        {
+            path: 'PlayContentMsg',
+            name: 'PlayContentMsg',
+            component:PlayContentMsg,
+
+        },
+        {
+            path: 'PlayManyPicturesContentMsg',
+            name: 'PlayManyPicturesContentMsg',
+            component:PlayManyPicturesContentMsg,
+
+        },
+        {
+            path: 'PlayNewsMsg',
+            name: 'PlayNewsMsg',
+            component:PlayNewsMsg,
+
+        },
+        {
+            path: 'PlayPictrueMsg',
+            name: 'PlayPictrueMsg',
+            component:PlayPictrueMsg,
+
+        },
+        {
+            path: 'PlayVideosMsg',
+            name: 'PlayVideosMsg',
+            component:PlayVideosMsg,
+
+        },
+
+
+
         {
             path: 'FormConfig/createproject',
             name: 'createproject',
             component: createproject,
         },
+
+
+
         {
             path: 'analysis_word',
             name: 'analysis_word',
@@ -224,6 +267,13 @@ export default[
     {
         path:'/prostage',//第一套模板
         component:()=>import('../views/prostage/index/index.vue')
+    },
+
+    {
+        path: '/ClientWeb/TextTemplate/FrameWork',
+        name: 'FrameWork',
+        component:FrameWork,
+
     },
 ]
 
