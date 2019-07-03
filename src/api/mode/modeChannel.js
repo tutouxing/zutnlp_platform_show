82bde4eb00d0fe66806b61bfeffc549d86f05937
xumenqi <1975668635@qq.com>
2019-07-03 09:36:47 +0800

    添加评论部分

--- src/api/mode/modeChannel.js
+++ src/api/mode/modeChannel.js
@@ -2,14 +2,14 @@ import fetch from "../../utils/fetch";
 
 export function getChannelById(id) {
     return fetch({
-        url:'http://202.196.37.147:8848/Channel/'+id,
+        url:'http://127.0.0.1:8848/Channel/'+id,
         method:'get',
     })
 }
 
 export function getChannelAll(userId) {
     return fetch({
-        url:'http://202.196.37.147:8848/Channel/'+userId,
+        url:'http://127.0.0.1:8848/Channel/'+userId,
         method:'get',
     })
 }
